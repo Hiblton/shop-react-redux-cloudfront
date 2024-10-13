935f9da970960f8866ac4711f72ea54efd92491e
Slava Selivonchyk <slava.selivonchyk@gmail.com>
2024-10-13 19:05:03 +0200

    Set products

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            My Store!
+            Drum Store!
           </Link>
         </Typography>
 
