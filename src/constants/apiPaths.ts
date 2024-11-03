2b565fd6ea1055516ebc151bca0d48a853059689
Slava Selivonchyk <slava.selivonchyk@gmail.com>
2024-11-03 01:03:06 +0100

    Connect to import file endpoint

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://0mgcn6yyjd.execute-api.eu-central-1.amazonaws.com/prod",
   bff: "https://k0h09xn077.execute-api.eu-central-1.amazonaws.com/prod",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
